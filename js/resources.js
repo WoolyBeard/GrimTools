9ce87c2833ffb09f71c7a111e739ad36f7438c69
WoolyManBeard <manlybeardricardo@gmail.com>
2025-02-14 23:14:36 -0400

    Fix Resources Push
    Fix

---
+++ js/resources.js
@@ -0,0 +1,18 @@
+// data.js
+window.resources = [
+  { name: "Resources/pdf/Artificer.pdf", type: "file" },
+  { name: "Resources/pdf/Bard.pdf", type: "file" },
+  { name: "Resources/pdf/Berserker.pdf", type: "file" },
+  { name: "Resources/pdf/Cleric.pdf", type: "file" },
+  { name: "Resources/pdf/Druid.pdf", type: "file" },
+  { name: "Resources/pdf/Fighter.pdf", type: "file" },
+  { name: "Resources/pdf/Monk.pdf", type: "file" },
+  { name: "Resources/pdf/Paladin.pdf", type: "file" },
+  { name: "Resources/pdf/Psion.pdf", type: "file" },
+  { name: "Resources/pdf/Ranger.pdf", type: "file" },
+  { name: "Resources/pdf/Rogue.pdf", type: "file" },
+  { name: "Resources/pdf/Sorcerer.pdf", type: "file" },
+  { name: "Resources/pdf/Warlock.pdf", type: "file" },
+  { name: "Resources/pdf/Wizard.pdf", type: "file" },
+  { name: "Resources/Excalidraw Data/GrimwildQuickstart.excalidraw", type: "file"}
+];
